5bbe710b5950f7a0dc10ed2a29b39a38bafc24c2
Tom Sarry <tom.sarry@mail.mcgill.ca>
2023-02-04 17:03:01 -0500

    feat: add nav, add about page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script>
 	import './styles.css';
+	import { page } from '$app/stores';
 </script>
 
 <div class="app">
 	<header>
 		<h1>bip39typer</h1>
+		<nav>
+			<span aria-current={$page.url.pathname === '/' ? 'page' : undefined} class="nav-link"
+				><a href="/">typer</a></span
+			>
+			<span aria-current={$page.url.pathname === '/about' ? 'page' : undefined} class="nav-link"
+				><a href="/about">about</a></span
+			>
+		</nav>
 	</header>
 
 	<slot />
@@ -23,10 +32,49 @@
 		color: #313131;
 	}
 
+	h1 {
+		margin: 20px 0 10px 0;
+		font-size: 3em;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.nav-link {
+		display: inline-block;
+		margin: 0px 8px;
+		border-radius: 8px;
+		transition: background-color 0.2s ease;
+	}
+
+	.nav-link:hover {
+		background-color: #cecece;
+	}
+
+	.nav-link[aria-current='page'] {
+		background-color: #313131;
+		color: #ebebeb;
+	}
+
+	.nav-link[aria-current='page'] a {
+		color: white;
+	}
+
+	a {
+		color: #313131;
+		font-weight: 400;
+		font-size: 1.5em;
+		padding: 0px 14px;
+		text-decoration: none;
+	}
+
 	header {
 		width: 100vw;
 		display: flex;
-		flex-direction: row;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 	}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,44 @@
+<div class="about">
+	<h2>About</h2>
+
+	<p>
+		This website is a little toy project I made over the week-end. It was so cold out (-40 degrees
+		Celcius), I had to find something to keep me busy inside and so here it is: a <b
+			>BIP39 word typer</b
+		> using SvelteKit.
+	</p>
+	<p>
+		For those of you that do not know, <a
+			href="https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki">BIP39</a
+		>
+		is the <i>Bitcoin Improvement Proposal</i> that suggested we use a mnemonic code composed of english
+		words (the seed words) to have a more user-friendly way to generate deterministic wallets.
+	</p>
+
+	<p>
+		BIP39 therefore uses a collection of 2048 words, that are unambiguous: the first four letters
+		are sufficient to uniquely determine a word and where pairs of similar words are avoided (woman
+		vs women for example). If you're wondering what these words are, simply start <a href="/"
+			>typing</a
+		>!
+	</p>
+</div>
+
+<style>
+	h2 {
+		text-align: center;
+	}
+	.about {
+		max-width: 600px;
+		min-height: 80vh;
+		text-align: justify;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+	}
+
+	a {
+		color: #313131;
+	}
+</style>
